3bfc6bc9d8a713817746f4a7e4844eeb667c42f3
jojotier <122423763+jojotier@users.noreply.github.com>
2023-10-10 14:37:31 -0500

    finished code-along from model-binding chapter

--- Java_Practice/codingevents/src/main/java/org/launchcode/codingevents/Controllers/EventController.java
+++ Java_Practice/codingevents/src/main/java/org/launchcode/codingevents/Controllers/EventController.java
@@ -1,11 +1,10 @@
 package org.launchcode.codingevents.Controllers;
 
+import org.launchcode.codingevents.Models.Event;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.launchcode.codingevents.Data.EventData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,12 +13,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -30,8 +27,27 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        EventData.addEvent(newEvent);
+        return "redirect:/events";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model){
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required=false) int[] eventIds){
+
+        if(eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+
         return "redirect:/events";
     }
 
---
+++ Java_Practice/codingevents/src/main/java/org/launchcode/codingevents/Data/EventData.java
@@ -0,0 +1,32 @@
+package org.launchcode.codingevents.Data;
+
+import org.launchcode.codingevents.Models.Event;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EventData {
+
+    private static final Map<Integer, Event> events = new HashMap<>();
+
+    //add an event
+    public static void addEvent(Event event){
+        events.put(event.getId(), event);
+    }
+    // get a single event
+    public static Event getById(int id){
+        return events.get(id);
+    }
+    //get all events
+    public static Collection<Event> getAll(){
+        return events.values();
+    }
+    //remove an event
+    public static void remove(int id){
+        events.remove(id);
+    }
+
+
+
+}
