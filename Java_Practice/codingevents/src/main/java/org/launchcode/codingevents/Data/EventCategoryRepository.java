02f1fc546a7f115e4e175dc8c5767769867ccd74
jojotier <122423763+jojotier@users.noreply.github.com>
2023-11-01 13:31:40 -0500

    completed the ORM exercises!

---
+++ Java_Practice/codingevents/src/main/java/org/launchcode/codingevents/Data/EventCategoryRepository.java
@@ -0,0 +1,8 @@
+package org.launchcode.codingevents.Data;
+
+import org.launchcode.codingevents.Models.EventCategory;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventCategoryRepository extends CrudRepository<EventCategory, Integer> {
+
+}
